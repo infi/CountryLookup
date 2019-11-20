070b079e0c35d279748d3c1207036f81d5edafed
Infi <sp46alt@yahoo.com>
2019-11-20 21:05:06 +0100

    Initial MVP

---
+++ README.md
@@ -0,0 +1,51 @@
+# CountryLookup
+  
+## Run  
+  
+`npm i`. Now you can deploy, preferably the entire thing.
+
+The following text are the answers for the initial questionnaire for participation in Seedling School.
+
+## Elevator Pitch
+
+CountryLookup helps you to find the most important information about a country without reading an entire Wikipedia page. Among others, things like languages, currencies, and even demonyms (Demonym Example: Germany => German, Italy => Italian).
+
+## What is your motivation for creating this project?
+
+First of all, I want to improve my personal frameworkless skills, and provide a friendly frontend to REST Countries, the API I am going to be using.
+
+## How will users interact with your web site?
+  
+A simple search bar, the user will get back cards with requested data.  
+It's live updating, but respecting the ratelimits.
+
+## What 3rd Party API(s) will you integrate with?
+
+At the moment, [REST Countries](https://restcountries.eu/).
+
+## Tech Stack
+
+> CSS Framework will you use - or no framework  
+  
+Bulma, installed via npm
+> Any 3rd party JS libraries like maps, data viz etc.  
+  
+REST Countries doesn't seem to serve any visualisable data, however it seems like I could use a map to show the position of the country on Earth. 
+
+## What will be your process?
+
+> What project management tool will you use?  
+  
+An internal Taiga (Trello-like) board.  
+> How often will you commit your code?  
+  
+
+I usually divide my coding in unlimited-time "sessions". A session is starting and stopping when I feel like it. Code should be commited when the MVP is ready, and at the end of every session from that moment on. Hotfixes, like ones that fix a thing being absolutely broken, are commited ASAP.  
+
+> How will you test your web site?  
+  
+By testing it in Firefox, then Chromium. I am not going to implement any E2E or Unit tests due to lack of knowledge on that matter.  
+  
+> How will you design the layout of your website? Will you use a wireframing tool? Will you draw it on paper?  
+  
+I have created a wireframe with Inkscape (= svg), it's in the root of this repository as `wireframe.svg`.
---
+++ lookup.js
@@ -0,0 +1,114 @@
+const sel = (q) => document.querySelector(q)
+
+const searchbar = sel("#searchbox")
+const searchbtn = sel("#searchbtn")
+
+searchbar.value = ""
+
+const resultContainer = {
+    self: sel("#result-container"),
+    children: {
+        name: sel("#result-name"),
+        flag: sel("#result-flag"),
+        tlds: sel("#result-tlds"),
+        codes: sel("#result-codes"),
+        callc: sel("#result-callc"),
+        capi: sel("#result-capi"),
+        aka: sel("#result-aka"),
+        regio: sel("#result-regio"),
+        pop: sel("#result-pop"),
+        demonym: sel("#result-demonym"),
+        time: sel("#result-time"),
+        border: sel("#result-border"),
+        native: sel("#result-native"),
+        numeric: sel("#result-numeric"),
+        lang: sel("#result-lang")
+    }
+}
+
+const errorContainer = {
+    self: sel("#error-container"),
+    title: sel("#error-title"),
+    desc: sel("#error-desc")
+}
+
+resultContainer.self.style.display = "none"
+errorContainer.self.style.display = "none"
+
+/**
+ * initializes a search
+ * @param {String} query
+ * @returns {void} 
+ */
+const initializeSearch = async (query) => {
+    if (!query) return error("Searches cannot be empty", "Fun fact: This tool supports about 240 countries.")
+    resultContainer.self.style.display = "none"
+    errorContainer.self.style.display = "none"
+    const res = await fetch("https://restcountries.eu/rest/v2/name/" + String(query))
+    const body = await res.json().catch(() => {
+        error("Failed to parse response", "Please make sure your query is valid.")
+    })
+
+    const country = body[0]
+    if (!country) {
+        error("Not found", "No results were found to your query.")
+    } else {
+        resultContainer.self.style.display = "inherit"
+
+        // header
+        resultContainer.children.name.innerText = country.name
+        resultContainer.children.flag.setAttribute("src", country.flag)
+
+        // column 1
+        resultContainer.children.tlds.innerText = country.topLevelDomain.join(", ") || ""
+        resultContainer.children.codes.innerText = `2-Char: ${country.alpha2Code} / 3-Char: ${country.alpha3Code}`
+        resultContainer.children.callc.innerText = country.callingCodes.map(x => "+" + x).join(", ") || ""
+        resultContainer.children.capi.innerText = country.capital
+
+        // column 2
+        resultContainer.children.aka.innerText = country.altSpellings.join(", ") || ""
+        resultContainer.children.regio.innerText = country.region
+        resultContainer.children.pop.innerText = country.population
+        resultContainer.children.demonym.innerText = country.demonym
+
+        // column 3
+        // just 2 sections, as those can get long already
+        resultContainer.children.time.innerText = "UTC: " + country.timezones
+            .map(x =>
+                x // remove unneeded info
+                    .replace("UTC", "")
+                    .replace(":00", "")
+            ).join(", ")
+            || ""
+        resultContainer.children.border.innerText = country.borders.join(", ") || "<nobody>"
+
+        // column 4
+        resultContainer.children.native.innerText = country.nativeName
+        resultContainer.children.numeric.innerText = country.numericCode
+        let languages = []
+        country.languages.forEach(language => {
+            languages.push(language.name)
+        })
+        resultContainer.children.lang.innerText = languages.join(", ") || ""
+    }
+}
+
+searchbar.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+        initializeSearch(searchbar.value)
+    }
+})
+searchbtn.addEventListener("click", () => [
+    initializeSearch(searchbar.value)
+])
+
+/**
+ * Display an error in a human-intended format
+ * @param {String} title Error title 
+ * @param {String} desc Error description
+ */
+const error = (title, desc) => {
+    errorContainer.self.style.display = "inherit"
+    errorContainer.desc.innerText = desc
+    errorContainer.title.innerText = title
+}
